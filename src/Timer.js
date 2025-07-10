ada213b8ca313347884f671a2402f3701e84eeda
r.azadi <siavashirani001@gmail.com>
2025-07-10 10:48:58 +0330

    چرخه حیات

--- src/Timer.js
+++ src/Timer.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.css'
-
+ var interval;
 class Timer extends React.Component
 {
+
   constructor(){
     super();
     this.state={
@@ -11,12 +12,31 @@ class Timer extends React.Component
 
     }
   }
-  render(){
-    setInterval(() => {
+
+  componentDidMount(){
+    console.log("componentDidMount");
+   interval=  setInterval(() => {
       this.setState({
        time: new Date().toLocaleTimeString()
       })
     }, 1000);
+  }
+
+  componentDidUpdate (){
+    if(this.state.time == "10:45:30 AM")
+    {
+        clearInterval(interval);
+    }
+
+  }
+
+
+  componentWillUnmount(){
+    console.log("componentWillUnmount");
+  }
+  render(){
+    console.log("render");
+
     return(
       <h2 className='timer'>
           it is {this.state.time}
