3c7871276b86c30b27244fff25bfed3252f026fb
r.azadi <siavashirani001@gmail.com>
2025-07-10 07:24:16 +0330

    ساخت کامپوننت

--- src/index.js
+++ src/index.js
@@ -2,24 +2,47 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
 
 
-
-
-
-const tick=()=>
-{
-  const elem=(
-    <div>
+class Hello extends React.Component {
+  render(){
+    return(
       <h1>سلام دوستان</h1>
-      <h2>
-         it is {new Date().toLocaleTimeString()}
-      </h2>
-    </div>
-  )
-  const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render( elem);
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+         <Hello/>
+         <Timer/>
+      </div>
+    );
+  }
+}
+
+
+class Timer extends React.Component
+{
+  render(){
+    return(
+      <h2>
+          it is {new Date().toLocaleTimeString()}
+      </h2>
+    );
+  }
 
 }
+
+
+const tick = ()=> {
+    const root = ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
+
+
+
+}
+
 
 setInterval(() => {
   tick();
